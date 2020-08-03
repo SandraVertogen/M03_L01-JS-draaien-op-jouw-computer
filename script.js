64712ba2fb90db51935732e53ebf5ea4c45eb40a
SandraVertogen <65301809+SandraVertogen@users.noreply.github.com>
2020-08-03 12:23:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Hallo Winc Academy studenten");
+
+const getal1 = 1230941;
+const getal2 = 1823794;
+/*  const product = getal1 * getal2;
+//dit is een grote som
+console.log(product);
+//de comment is niet zichtbaar, de uitkomst wel, want deze staat niet in de comment
+
+console.log(637263 / 54); */
+//nu zijn alle uitkomsten niet meer zichtbaar, omdat  de  uitkomsten  nu wel in de comment staan
